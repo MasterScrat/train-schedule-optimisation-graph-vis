87639cf745fe647a85ad87a56cedd182e0ff60ca
flaurent <florian.laurent@gmail.com>
2018-09-07 17:46:15 +0200

    Cleanup

--- export_instance.py
+++ export_instance.py
@@ -1,6 +1,10 @@
 import json
 
-scenario = "instances/sample_scenario.json"
+#scenario = "instances/sample_scenario.json"
+scenario = "instances/01_dummy.json"
+show_markers = False
+show_resources = True
+nb_routes_shown = 10
 
 def from_node_id(route_path, route_section, index_in_path):
     if "route_alternative_marker_at_entry" in route_section.keys() and \
@@ -36,26 +40,35 @@ with open(scenario) as fp:
 nodes = []
 edges = []
 
-for route in scenario["routes"]:
+node_to_marker = {}
+
+for route in scenario["routes"][0:nb_routes_shown]:
     for path in route["route_paths"]:
         for (i, route_section) in enumerate(path["route_sections"]):
             section_markers = route_section['section_marker'] if 'section_marker' in route_section else {}
 
-            """
-            for section_marker in section_markers:
-                nodes.append({'id': section_marker+'_sm', 'color': 'red', 'shape': 'box', 'label': section_marker})
-                edges.append({'from': section_marker+'_sm', 'to': str(route["id"]) + to_node_id(path, route_section, i), 'dashes': 'true'})
-            """
+            if show_markers:
+                for section_marker in section_markers:
+                    nodes.append({'id': section_marker+'_sm', 'color': 'red', 'shape': 'box', 'label': section_marker})
+                    #edges.append({'from': section_marker+'_sm', 'to': str(route["id"]) + to_node_id(path, route_section, i), 'dashes': 'true'})
+                    node_to_marker[section_marker+'_sm'+'->'+str(route["id"]) + to_node_id(path, route_section, i)] = {'from': section_marker+'_sm', 'to': str(route["id"]) + to_node_id(path, route_section, i), 'dashes': 'true'}
 
-            for resource_occupations in route_section['resource_occupations']:
-                resource = resource_occupations['resource']
-                nodes.append({'id': resource+'_ro', 'color': 'blue', 'shape': 'circle', 'label': resource})
-                edges.append({'from': resource+'_ro', 'to': str(route["id"])+to_node_id(path, route_section, i), 'dashes': 'true'})
+            if show_resources:
+                for resource_occupations in route_section['resource_occupations']:
+                    resource = resource_occupations['resource']
+                    nodes.append({'id': resource+'_ro', 'color': 'red', 'shape': 'circle', 'label': resource})
+                    edges.append({'from': resource+'_ro', 'to': str(route["id"])+to_node_id(path, route_section, i), 'dashes': 'true'})
 
 
             edges.append({'label': str(route_section['sequence_number']), 'from': str(route["id"])+from_node_id(path, route_section, i), 'to': str(route["id"])+to_node_id(path, route_section, i), 'arrows': {'to': {'enabled': True, 'scaleFactor': 3}}, 'width': 5})
             nodes.append({'id': str(route["id"])+from_node_id(path, route_section, i), 'label': from_node_id(path, route_section, i), 'color': id_to_color(route["id"]), 'shape': 'ellipsis'})
             nodes.append({'id': str(route["id"])+to_node_id(path, route_section, i), 'label': to_node_id(path, route_section, i), 'color': id_to_color(route["id"]), 'shape': 'ellipsis'})
+
+for marker_edge in node_to_marker:
+    #print(marker_edge)
+    edges.append(node_to_marker[marker_edge])
+
+#print(node_to_marker)
 
 # removes duplicate nodes
 nodes = list({v['id']: v for v in nodes}.values())
